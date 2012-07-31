aa91de179bf02bdf4b3c27dc5fc651f815a45d4b
Keenan May <krmpro@gmail.com>
2012-07-31 13:14:33 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Redux::Application.config.secret_token = '594e29ae806eb7b96cd3dd6775995ac5753b6d6aa9d8fb44353fee1d7cc77f53434f83a5f451e76807686f39e5a5696d38f5341f4c857c2f8035ef47d0b9421e'
